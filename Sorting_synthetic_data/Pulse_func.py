a096bfa6060082dc3abbe3d3846d5119eb287d3b
Zeinab-Mohammadi <76111159+Zeinab-Mohammadi@users.noreply.github.com>
2022-11-08 14:05:36 -0500

    Update Pulse_func.py

--- Sorting_synthetic_data/Pulse_func.py
+++ Sorting_synthetic_data/Pulse_func.py
@@ -1,4 +1,4 @@
-### This function i to makeing a poission generator
+### Makeing a poission generator
 
 # fr: is the considered firing rate
 # duration: duration of the produced pulses
@@ -8,19 +8,17 @@ import random
 
 def produce_pulses(fr, total_length, time_interval):
     random.seed()
-    fr_time = 1.0/fr
+    fr_time = 1.0 / fr
     num = int(total_length/time_interval)
-    
     pulses = np.zeros(num, dtype=np.float)
-    
     k = 0
-    inv_interval = 1/time_interval
+    inv_interval = 1 / time_interval
     while k < num:
         ran = random.random()
-        diff = -fr_time*np.log(ran)*inv_interval
+        diff = -fr_time * np.log(ran) * inv_interval
         k += int(diff)
         if k < num:
-            pulses[k]=pulses[k]+1.0
+            pulses[k] = pulses[k] + 1.0
             
     return pulses
 
