94e98720bb6bdedc4868b37f69f577fe41525a0e
Zeinab-Mohammadi <76111159+Zeinab-Mohammadi@users.noreply.github.com>
2022-11-08 14:06:16 -0500

    Update Smoothing_data.py

--- Sorting_synthetic_data/Smoothing_data.py
+++ Sorting_synthetic_data/Smoothing_data.py
@@ -1,5 +1,5 @@
 
-#This is the Smoothing function
+#This is the smoothing function
 
 import copy
 import numpy as np
@@ -7,11 +7,11 @@ from scipy.signal import butter, lfilter
 
 def Smoothing_filter(data_raw, low, high, sf, order):
     # Determine Nyquist frequency
-    nyq_freq = sf/2
+    nyq_freq = sf / 2
 
     # Set bands
-    low = low/nyq_freq
-    high = high/nyq_freq
+    low = low / nyq_freq
+    high = high / nyq_freq
 
     # Calculate coefficients
     b, a = butter(order, [low, high], btype='band')
